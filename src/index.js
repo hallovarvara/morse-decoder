04edbba45468a79fefb4c9e422e052f25b0158c0
Varya Deviaterikova <varvara_deviaterikova@epam.com>
2021-02-05 20:16:31 +0300

    refactor: refactor task solution

--- src/index.js
+++ src/index.js
@@ -1,65 +1,84 @@
-const MORSE_TABLE = {
-    '.-':     'a',
-    '-...':   'b',
-    '-.-.':   'c',
-    '-..':    'd',
-    '.':      'e',
-    '..-.':   'f',
-    '--.':    'g',
-    '....':   'h',
-    '..':     'i',
-    '.---':   'j',
-    '-.-':    'k',
-    '.-..':   'l',
-    '--':     'm',
-    '-.':     'n',
-    '---':    'o',
-    '.--.':   'p',
-    '--.-':   'q',
-    '.-.':    'r',
-    '...':    's',
-    '-':      't',
-    '..-':    'u',
-    '...-':   'v',
-    '.--':    'w',
-    '-..-':   'x',
-    '-.--':   'y',
-    '--..':   'z',
-    '.----':  '1',
-    '..---':  '2',
-    '...--':  '3',
-    '....-':  '4',
-    '.....':  '5',
-    '-....':  '6',
-    '--...':  '7',
-    '---..':  '8',
-    '----.':  '9',
-    '-----':  '0',
-};
-
 function decode(expr) {
+  const MORSE_TABLE = {
+    '.-': 'a',
+    '-...': 'b',
+    '-.-.': 'c',
+    '-..': 'd',
+    '.': 'e',
+    '..-.': 'f',
+    '--.': 'g',
+    '....': 'h',
+    '..': 'i',
+    '.---': 'j',
+    '-.-': 'k',
+    '.-..': 'l',
+    '--': 'm',
+    '-.': 'n',
+    '---': 'o',
+    '.--.': 'p',
+    '--.-': 'q',
+    '.-.': 'r',
+    '...': 's',
+    '-': 't',
+    '..-': 'u',
+    '...-': 'v',
+    '.--': 'w',
+    '-..-': 'x',
+    '-.--': 'y',
+    '--..': 'z',
+    '.----': '1',
+    '..---': '2',
+    '...--': '3',
+    '....-': '4',
+    '.....': '5',
+    '-....': '6',
+    '--...': '7',
+    '---..': '8',
+    '----.': '9',
+    '-----': '0',
+  };
+
+  const SPACE_CODE = '**********';
+  const SPACE = ' ';
+  const DOT_CODE = '10';
+  const DOT = '.';
+  const DASH_CODE = '11';
+  const DASH = '-';
+  let decodedText = '';
   let startPos = 0;
-  let decoded = "";
-  for (let i = 0; i < expr.length/10; i++) {
-    let letterNums = expr.slice(startPos, startPos + 10);
-    startPos += 10;
-    if (letterNums == "**********") {
-      decoded += " ";
+
+  const checkForSpecialSymbols = (symbols) => {
+    switch (symbols) {
+      case DOT_CODE:
+        return DOT;
+      case DASH_CODE:
+        return DASH;
+      default:
+        return '';
+    }
+  };
+
+  for (let i = 0; i < expr.length / SPACE_CODE.length; i++) {
+    let letterNumbers = expr.slice(startPos, startPos + SPACE_CODE.length);
+    startPos += SPACE_CODE.length;
+
+    if (letterNumbers === SPACE_CODE) {
+      decodedText += SPACE;
       continue;
     }
-    let morsed = "";
-    for (let j = 10; j > 0; j -= 2) {
-      switch( letterNums.slice(j-2, j) ) {
-        case "10" : morsed += "."; break;
-        case "11" : morsed += "-"; break; 
-        default: break;
-      }
+
+    let decodedSymbols = '';
+
+    for (let j = SPACE_CODE.length; j > 0; j -= 2) {
+      decodedSymbols += checkForSpecialSymbols(letterNumbers.slice(j - 2, j));
     }
-    decoded += MORSE_TABLE[ morsed.split("").reverse().join("") ];
+
+    decodedText += MORSE_TABLE[decodedSymbols.split('').reverse().join('')];
   }
-  return decoded;
+
+  return decodedText;
 }
 
 module.exports = {
-    decode
-}
+  decode,
+};
